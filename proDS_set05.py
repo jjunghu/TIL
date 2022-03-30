52838ba749399afbf7b2e410534900a84d57edb3
jjunghu <namgungjunghu@gmail.com>
2022-03-30 23:11:42 +0900

    upload TIL 220330

---
+++ proDS_set05.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 30 22:47:01 2022
+
+@author: jjunghu
+"""
+
+#%%
+
+# =============================================================================
+# =============================================================================
+# # 문제 05 유형(DataSet_05.csv 이용)
+#
+# 구분자 : comma(“,”), 8,068 Rows, 12 Columns, UTF-8 인코딩
+#
+# A자동차 회사는 신규 진입하는 시장에 기존 모델을 판매하기 위한 마케팅 전략을 
+# 세우려고 한다. 기존 시장과 고객 특성이 유사하다는 전제 하에 기존 고객을 세분화하여
+# 각 그룹의 특징을 파악하고, 이를 이용하여 신규 진입 시장의 마케팅 계획을 
+# 수립하고자 한다. 다음은 기존 시장 고객에 대한 데이터이다.
+#
+
+# 컬 럼 / 정 의 / Type
+# ID / 고유 식별자 / Double
+# Age / 나이 / Double
+# Age_gr / 나이 그룹 (10/20/30/40/50/60/70) / Double
+# Gender / 성별 (여성 : 0 / 남성 : 1) / Double
+# Work_Experience / 취업 연수 (0 ~ 14) / Double
+# Family_Size / 가족 규모 (1 ~ 9) / Double
+# Ever_Married / 결혼 여부 (Unknown : 0 / No : 1 / Yes : 2) / Double
+# Graduated / 재학 중인지 여부 / Double
+# Profession / 직업 (Unknown : 0 / Artist ~ Marketing 등 9개) / Double
+# Spending_Score / 소비 점수 (Average : 0 / High : 1 / Low : 2) / Double
+# Var_1 / 내용이 알려지지 않은 고객 분류 코드 (0 ~ 7) / Double
+# Segmentation / 고객 세분화 결과 (A ~ D) / String
+# =============================================================================
+# =============================================================================
+
+
+#(참고)
+#1
+# import pandas as pd
+# #2
+# from scipy.stats import chi2_contingency
+# #3
+# from sklearn.model_selection import train_test_split
+# from sklearn import metrics
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.tree import export_graphviz
+# import pydot
+
+
+#%%
+
+import pandas as pd
+import numpy as np
+
+data5 = pd.read_csv('./dataset/Dataset_05.csv', na_values=['?',' ','NA'])
+data5.info()
+data5.columns
+# ['ID', 'Age', 'Age_gr', 'Gender', 'Work_Experience', 'Family_Size',
+#        'Ever_Married', 'Graduated', 'Profession', 'Spending_Score', 'Var_1',
+#        'Segmentation']
+
+# =============================================================================
+# 1.위의 표에 표시된 데이터 타입에 맞도록 전처리를 수행하였을 때, 데이터 파일 내에
+# 존재하는 결측값은 모두 몇 개인가? 숫자형 데이터와 문자열 데이터의 결측값을
+# 모두 더하여 답하시오.
+# (String 타입 변수의 경우 White Space(Blank)를 결측으로 처리한다) (답안 예시) 123
+# =============================================================================
+
+data5.isnull().sum().sum()
+
+# 답: 1166
+
+
+#%%
+
+# =============================================================================
+# 2.이어지는 분석을 위해 결측값을 모두 삭제한다. 그리고, 성별이 세분화(Segmentation)에
+# 영향을 미치는지 독립성 검정을 수행한다. 수행 결과, p-value를 반올림하여 소수점
+# 넷째 자리까지 쓰고, 귀무가설을 기각하면 Y로, 기각할 수 없으면 N으로 기술하시오. 
+# (답안 예시) 0.2345, N
+# =============================================================================
+
+q2 = data5.dropna()
+
+q2_tab = pd.crosstab(index = q2.Gender, columns = q2.Segmentation)
+
+from scipy.stats import chi2_contingency
+
+q2_out = chi2_contingency(q2_tab)
+
+round(q2_out[1], 4)
+round(q2_out[1], 4) < 0.05
+
+# 답: 0.0031, Y
+
+#%%
+
+# =============================================================================
+# 3.Segmentation 값이 A 또는 D인 데이터만 사용하여 의사결정 나무 기법으로 분류
+# 정확도를
+# 측정해 본다. 
+# - 결측치가 포함된 행은 제거한 후 진행하시오.
+# - Train대 Test 7대3으로 데이터를 분리한다. (Seed = 123)
+# - Train 데이터를 사용하여 의사결정나무 학습을 수행하고, Test 데이터로 평가를
+# 수행한다.
+# - 의사결정나무 학습 시, 다음과 같이 설정하시오:
+# • Feature: Age_gr, Gender, Work_Experience, Family_Size, 
+#             Ever_Married, Graduated, Spending_Score
+# • Label : Segmentation
+# • Parameter : Gini / Max Depth = 7 / Seed = 123
+# 이 때 전체 정확도(Accuracy)를 소수점 셋째 자리 이하는 버리고 소수점 둘째자리까지
+# 기술하시오.
+# (답안 예시) 0.12
+# =============================================================================
+
+q3 = q2[q2.Segmentation.isin(['A', 'D'])]
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+train, test = train_test_split(q3, test_size=0.3, random_state=123)
+
+var_list = ['Age_gr', 'Gender', 'Work_Experience', 'Family_Size',
+            'Ever_Married', 'Graduated', 'Spending_Score']
+
+dt = DecisionTreeClassifier(max_depth=7, random_state=123)
+dt.fit(train[var_list], train['Segmentation'])
+q3_out = dt.score(test[var_list], test['Segmentation'])
+
+# 답: 0.68
